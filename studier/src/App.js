961910249848b6a48719a5717305b5eea66e1624
theSaab <saba.fathi@emailgmail.info>
2023-11-17 20:33:18 -0500

    des

--- studier/src/App.js
+++ studier/src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import { GoogleLogin } from "@react-oauth/google";
+import { googleLogout, useGoogleLogin } from "@react-oauth/google";
+import { auth, db, logout } from "./services/firebase.js";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { BrowserRouter as Router, Route, Routes, Form } from "react-router-dom";
+import SignIn from "./components/SignIn/SignIn";
+import LandingPage from "./components/LandingPage/LandingPage";
+import ActivityPage from "./components/ActivityPage/ActivityPage";
+import Navbar from "./components/Navbar/Navbar";
 
-function App() {
+const App = () => {
+  const [user, loading, error] = useAuthState(auth);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="App">
+        <Router>
+          <Routes>
+            <Route exact path="/" element={<SignIn />} />
+            <Route exact path="/landingpage" element={<LandingPage />} />
+            <Route exact path="/activity" element={<ActivityPage />} />
+          </Routes>
+          <Navbar />
+        </Router>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
