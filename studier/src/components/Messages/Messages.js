06e7068bc7cd90f8cffc5c196d5023106d2f4a00
theSaab <saba.fathi@emailgmail.info>
2023-11-18 17:20:19 -0500

    back to chat

--- studier/src/components/Messages/Messages.js
+++ studier/src/components/Messages/Messages.js
@@ -55,6 +55,7 @@ const Messages = ({ currentUserUid, matchUid }) => {
       </div>
       <div className="input-container">
         <input
+          className="text-field"
           type="text"
           value={newMessage}
           onChange={(e) => setNewMessage(e.target.value)}
