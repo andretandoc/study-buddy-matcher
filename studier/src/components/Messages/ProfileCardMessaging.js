f7deb99bb1636147ea2ac031a10bc00c56bf7aa0
theSaab <saba.fathi@emailgmail.info>
2023-11-19 11:01:14 -0500

    final bug ifx

--- studier/src/components/Messages/ProfileCardMessaging.js
+++ studier/src/components/Messages/ProfileCardMessaging.js
@@ -3,7 +3,7 @@ import { fetchUserDetails } from "../../services/firebase";
 import "./ProfileCardMessaging.css";
 
 const ProfileCardMessaging = ({ selected }) => {
-  const [userData, setUserData] = useState("");
+  const [userData, setUserData] = useState(null);
 
   const fetchUserData = async () => {
     try {
@@ -19,37 +19,47 @@ const ProfileCardMessaging = ({ selected }) => {
   }, [selected]);
 
   return (
-    <div id={`profileCard_${userData.uid}`} className={`ProfileCardMessaging`}>
-      <div className="ProfileCardHeader">
-        <h3>{userData.name}</h3>
-      </div>
-      <div className="ProfileCardBody">
-        <div className="ProfileCardInfo">
-          <p>
-            <strong>U{userData.yearsOfStudy}</strong>
-          </p>
-          <p>
-            <strong>Major:</strong> {userData.major}
-          </p>
-        </div>
-        <div className="ProfileCardCourses">
-          <p>
-            <strong>Past Courses:</strong> {userData.pastCourses.join(", ")}
-          </p>
-          <p>
-            <strong>Current Courses:</strong>{" "}
-            {userData.currentCourses.join(", ")}
-          </p>
-        </div>
-        <div className="ProfileCardDescription">
-          <p>{userData.description}</p>
-        </div>
-        {userData.image && (
-          <div className="ProfileCardImage">
-            <img src={`data:image/png;base64, ${userData.image}`} alt="User" />
+    <div id={`profileCard_${userData?.uid}`} className={`ProfileCardMessaging`}>
+      {userData ? (
+        <>
+          <div className="ProfileCardHeader">
+            <h3>{userData.name}</h3>
           </div>
-        )}
-      </div>
+          <div className="ProfileCardBody">
+            <div className="ProfileCardInfo">
+              <p>
+                <strong>U{userData.yearsOfStudy}</strong>
+              </p>
+              <p>
+                <strong>Major:</strong> {userData.major}
+              </p>
+            </div>
+            <div className="ProfileCardCourses">
+              <p>
+                <strong>Past Courses:</strong>{" "}
+                {userData?.pastCourses.join(", ")}
+              </p>
+              <p>
+                <strong>Current Courses:</strong>{" "}
+                {userData?.currentCourses.join(", ")}
+              </p>
+            </div>
+            <div className="ProfileCardDescription">
+              <p>{userData.description}</p>
+            </div>
+            {userData.image && (
+              <div className="ProfileCardImage">
+                <img
+                  src={`data:image/png;base64, ${userData.image}`}
+                  alt="User"
+                />
+              </div>
+            )}
+          </div>
+        </>
+      ) : (
+        <p>Loading user data...</p>
+      )}
     </div>
   );
 };
